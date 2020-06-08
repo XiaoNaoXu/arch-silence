911f99dae005e0da086d04193fdf6dbd046215f3
陈红杰 <45337129+XiaoNaoXu@users.noreply.github.com>
2020-06-08 09:42:12 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+mkdir -p /boot/grub/themes/arch-silence
 rm -rf /boot/grub/themes/arch-silence
 cp -TR ./theme /boot/grub/themes/arch-silence
+echo "GRUB_THEME=/boot/grub/themes/arch-silence/theme.txt" >> /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
